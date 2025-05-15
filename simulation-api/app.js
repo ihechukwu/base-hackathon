5722acd8925bdccb193baf49781a64155efe79e1
Mavdavis <ekwesimarvelous@gmail.com>
2025-05-15 21:46:56 +0100

    allow loonpay

--- simulation-api/app.js
+++ simulation-api/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://base-hackathon-tau.vercel.app"],
+    origin: ["http://localhost:5173", "https://base-hackathon-tau.vercel.app", "https://www.loonpay.org/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
